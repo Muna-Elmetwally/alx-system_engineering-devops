f77d8e73028c278a3bc66e67660ccd674e524b48
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-04-20 05:32:10 +0200

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# installing a package using puppet
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+#!/usr/bin/pup
+# install a specific version of Flask (2.1.0)
+package { 'werkzeug':
+  ensure   => '2.0.2',
+  provider => 'pip3',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
